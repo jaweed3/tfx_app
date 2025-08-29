df88fbdd85e60b3d5de23ca1141b34a40331a9bc
jaweed3 <wedjaw22@gmail.com>
2025-08-29 14:34:55 +0700

    add: validation data

--- data_validation.py
+++ data_validation.py
@@ -3,6 +3,12 @@ import tensorflow_data_validation as tfdv
 stats = tfdv.generate_statistics_from_csv(
     data_location='/data/phishingData.csv',
     delimiter=','
+)
 
+# Generating Schema from the dataset
 schema = tfdv.infer_schema(stats)
 tfdv.display_schema(schema)
+
+# Visulaize Statistics
+tfdv.visualize_statistics(stats)
+
