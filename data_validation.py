227c9738be254ddf0f28369039a12acc34e6fc22
jaweed3 <wedjaw22@gmail.com>
2025-08-30 20:11:26 +0700

    add: wrap validation func

--- data_validation.py
+++ data_validation.py
@@ -1,46 +1,28 @@
 import tensorflow_data_validation as tfdv
 
-stats = tfdv.generate_statistics_from_csv(
-    data_location='data/phisingData.csv',
-    delimiter=','
-)
+def validate_data(example_artifact_path):
+    # Creating data statistics
+    data_stats = tfdv.generate_statistics_from_tfrecord(
+        data_location=example_artifact_path
+    )
 
-# Generating Schema from the dataset
-schema = tfdv.infer_schema(stats)
-tfdv.display_schema(schema)
+    # Creatting datta schema
+    data_schema = tfdv.infer_schema(
+        statistics=data_stats
+    )
+    tfdv.display_schema(schema=data_schema)
 
-# Visulaize Statistics
-tfdv.visualize_statistics(stats)
+    # Validate Statistics
+    anomalies = tfdv.validate_statistics(
+        statistics=data_stats,
+        schema=data_schema,
+    )
+    tfdv.display_anomalies(anomalies=anomalies)
 
-# Comparing dataset's statistics
-train_stats = tfdv.generate_statistics_from_csv(
-    data_location='/data/train/001.csv',
-    delimiter=','
-)
-val_stats = tfdv.generate_statistics_from_csv(
-    data_location='/data/train/001.csv',
-    delimiter=','
-)
+    tfdv.get_feature(data_schema, 'URL_length').drift_comparator.infinity_norm.threshold = 0.01
+    drift_anomalies = tfdv.validate_statistics(
+        statistics=data_stats,
+        schema=data_schema,
+    )
 
-# Visualize the comparison between those two
-tfdv.visualize_statistics(lhs_statistics=val_stats, rhs_statistics=train_stats,
-                          lhs_name='VAL_DATASET', rhs_name='TRAIN_DATASET')
-
-# Detect the Anomaly wih TFDV
-anomalies = tfdv.validate_statistics(statistics=val_stats, schema=schema)
-
-try: 
-    tfdv.display_anomalies(anomalies)
-except:
-    print("Error Occured")
-
-print(anomalies)
-
-tfdv.get_feature(schema, 'URL_length').drift_comparator.infinity_norm.threshold = 0.01
-drift_anomalies = tfdv.validate_statistics(
-    statistics=train_stats,
-    schema=schema,
-    previous_statistics=val_stats
-)
-
-print(drift_anomalies)
+    print(drift_anomalies)
--- pipeline.py
+++ pipeline.py
@@ -3,8 +3,9 @@ from tfx.orchestration.pipeline import Pipeline
 from data_ingestion import create_ingestion
 import os
 
-data_root = os.path.join(os.getcwd(), 'data')
 
+
+data_root = os.path.join(os.getcwd(), 'data')
 example_gen = create_ingestion(data_root)
 
 def create_pipeline(pipeline_root, metadata_path, data_root):
@@ -20,4 +21,8 @@ def create_pipeline(pipeline_root, metadata_path, data_root):
     ) 
 
 if __name__ == "__main__":
-    pipeline_root = os.path.join
+    pipeline_root = os.path.join(data_root, "pipeline")
+    metadata_path = os.path.join(data_root, "metadata")
+
+    pipeline = create_pipeline(pipeline_root, metadata_path, data_root)
+    print(pipeline)
